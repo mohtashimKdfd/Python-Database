34203b8d6453b173336caff1b88757b19b944c9b
mohtashimKdfd <mohtashim.kamran@unthinkable.co>
2021-12-22 16:20:11 +0530

    updating data in database

--- db.py
+++ db.py
@@ -14,7 +14,18 @@ cur=mydb.cursor()
 # print(cur)
 #execute command is used to execute any query
 # cur.execute("CREATE DATABASE t1")
-s='CREATE TABLE student(studentID integer(4), name varchar(30),class integer(10), height float(5,2))'
+# s='CREATE TABLE student(studentID integer(4), name varchar(30),class integer(10), height float(5,2))'
+#Already created Table
+s="INSERT INTO student (studentID,name,class,height) VALUES (%s,%s,%s,%s)"
+for _ in range(3):
+    sID = int(input('Enter the id of student:'))
+    naam = input('Enter the name of student:')
+    classs = int(input('Enter the class pf student'))
+    height  = input("Enter the height of student:")
+    st = (sID,naam,classs,height)
 
-cur.execute(s)
+    cur.execute(s,st)  #we can also pass arguments(objects that store our data to the execute command)
+
+    mydb.commit()   #commit function saves every changes made in our db
+
 print("Done")
